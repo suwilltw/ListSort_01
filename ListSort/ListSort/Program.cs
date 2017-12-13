2bc9351f81b7aa8a7fc2b28b476245e8e7d61707
Will Su <willsu@outlook.com>
2017-12-13 13:03:25 +0800

    1st version

---
+++ ListSort/ListSort/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            List<int> list = new List<int>();
+
+            //取出100個不重複之數字
+            int count = 1;
+            while (count <= 100)
+            {
+                int num = random.Next(1, 200);
+                if (list.Contains(num) == false)
+                {
+                    list.Add(num);
+                    count++;
+                }
+
+            }
+            Console.WriteLine("Original List:");
+            //print Original List
+            foreach (var item in list)
+            {
+                Console.Write(string.Format("{0}, ", item));
+            }
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+
+            list.Sort();
+            Console.WriteLine("Sorted List");
+            //print Sorted List
+            foreach (var item in list)
+            {
+                Console.Write(string.Format("{0}, ", item));
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
